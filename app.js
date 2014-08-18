fe6355d65a00a4cb34b27af40c6c695c0d137659
JadyaBird <jgodwin247@gmail.com>
2014-08-18 11:41:17 -0400

    gyroscope implementation added

--- app.js
+++ app.js
@@ -1,44 +1,79 @@
-var app = require('http').createServer(handler)
-  , url= require('url')
-  , fs = require('fs')
-  
-app.listen(5000);
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var fs = require('fs');
+var WebSocketServer = require('websocket').server;
+var piblaster = require("pi-blaster.js");
 
-// Http handler function
-function handler (req, res) {
-    
-    // Using URL to parse the requested URL
-    var path = url.parse(req.url).pathname;
-    
-    // Managing the root route
-    if (path == '/') {
-        index = fs.readFile(__dirname+'/public/index.html', 
-            function(error,data) {
-                
-                if (error) {
-                    res.writeHead(500);
-                    return res.end("Error: unable to load index.html");
-                }
-                
-                res.writeHead(200,{'Content-Type': 'text/html'});
-                res.end(data);
-            });
-    // Managing the route for the javascript files
-    } else if( /\.(js)$/.test(path) ) {
-        index = fs.readFile(__dirname+'/public'+path, 
-            function(error,data) {
-                
-                if (error) {
-                    res.writeHead(500);
-                    return res.end("Error: unable to load " + path);
-                }
-                
-                res.writeHead(200,{'Content-Type': 'text/plain'});
-                res.end(data);
-            });
-    } else {
-        res.writeHead(404);
-        res.end("Error: 404 - File not found.");
-    }
-    
-}
+
+var clients = [ ];
+
+//Getting request from browser
+//and send response back
+app.get ('/', function(req, res){    
+
+      fs.readFile('index.html', 'utf8', function(err, text){
+            res.send(text);
+        });
+});
+
+//Listening to Port 1337 for incoming messages
+server.listen(1337, function (){
+    console.log((new Date()) + " Server is listening on port 1337... ");
+});
+
+websock = new WebSocketServer({
+    httpServer: server
+});
+
+//WebSocket Server
+
+websock.on('request', function(request) {
+    
+    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
+
+    var connection = request.accept(null, request.origin);
+    
+    console.log((new Date()) + ' Connection accepted.');
+
+    //Incoming Data handling
+    connection.on('message', function(message) {
+
+        console.log('Data: ' +  message.utf8Data);
+   
+        var data = message.utf8Data;
+        data = data.slice(1,3);
+
+        // If incoming data is > 2 send a signal to pin 17
+        // Set GPIO pin 17 to a PWM of 24%
+        // Truns the Servo to it's right
+        if (Number(data)>2){        
+            piblaster.setPwm(17, 0.24);
+        }
+
+        // If incoming data is > 2 send a signal to pin 17
+        // Set GPIO pin 17 to a PWM of 6%
+        // Truns the Servo to it's left
+        if (Number(data)<(-2)){        
+            piblaster.setPwm(17, 0.06);
+        }
+
+        // If incoming data is > 2 send a signal to pin 17
+        // Set GPIO pin 17 to a PWM of 15%
+        // Truns the Servo to it's center position
+        if (Number(data)==0){        
+            piblaster.setPwm(17, 0.15);
+        }
+
+    });
+    
+    connection.on('close', function (connection){
+        //close connection
+        piblaster.setPwm(17, 0);
+    });
+
+    function closePin(){
+        piblaster.setPwm(17, 0);
+    }
+
+});
